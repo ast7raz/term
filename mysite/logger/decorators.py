8c3201704fbe829873dc937cb78ad1fd84ddb36b
Alex <ast7raz@gmail.com>
2016-11-10 13:08:28 +0200

    state master fix

--- mysite/logger/decorators.py
+++ mysite/logger/decorators.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
 import json
 from models import Logger_Action
+from ipware.ip import get_ip
 def Added_action_terminal(fun):
     def wraper_added(request, *args, **kwargs):
         req_slov=json.loads(request.body)
         user = User.objects.get(id=request.session.__getitem__('_auth_user_id'))
-        ip=request.META.get('REMOTE_ADDR', '') or request.META.get('HTTP_X_FORWARDED_FOR', '')
-
+        #ip=request.META.get('REMOTE_ADDR', '') or request.META.get('HTTP_X_FORWARDED_FOR', '')
+        ip=get_ip(request)
         for i in req_slov["ids"]:
 
             Logger_Action.objects.create(user=user, action_name=req_slov["command"], object_name="Terminal", object=i, user_ip=ip)
@@ -20,7 +21,8 @@ def Added_action_of_post(fun):
         user = User.objects.get(id=request.session.__getitem__('_auth_user_id'))
         if request.method == "POST":
             page=request.POST["page"].split("/")
-            ip = request.META.get('REMOTE_ADDR', '') or request.META.get('HTTP_X_FORWARDED_FOR', '')
+            #ip = request.META.get('REMOTE_ADDR', '') or request.META.get('HTTP_X_FORWARDED_FOR', '')
+            ip = get_ip(request, real_ip_only=True)
             response=fun(request)
             Logger_Action.objects.create(user=user, action_name=page[-1], action=response.content, object_name="Terminal", object=page[-2], user_ip=ip)
             return response
